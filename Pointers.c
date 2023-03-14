f184c5b62bc71c4a89adc5e72ededee58b1cce93
Vlad <94717630+Evac22@users.noreply.github.com>
2023-03-14 21:54:32 +0200

    Update Pointers.c

--- Pointers.c
+++ Pointers.c
@@ -1,8 +1,8 @@
-// подключение заголовочных файлов
+// including header files
 #include <stdio.h>
 #include <stdlib.h>
 
-// функция для перемещения нулей в конец массива
+// Function for moving zeros to the end of an array
 void move_zeros(int *arr, int size);
 
 int main() {
@@ -10,34 +10,34 @@ int main() {
     printf("Enter size of array: ");
     scanf("%d", &size);
     
-// выделение памяти под массив заданного размера
+// memory allocation for an array of a specified size
     int *arr = (int*) malloc(size * sizeof(int));
     int i;
 
     printf("Enter elements of array:\n");
-    // ввод элементов массива с клавиатуры
+// inputting array elements from keyboard// inputting array elements from keyboard
     for (i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
 
     printf("Original array:\n");
-    // вывод исходного массива
+  // printing the original array
     for (i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 
-// вызов функции для перемещения нулей в конец массива
+// calling the function to move zeros to the end of the array
     move_zeros(arr, size);
 
     printf("Array after moving zeros:\n");
-    // вывод массива после перемещения нулей в конец
+// Printing the array after moving zeros to the end
     for (i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
 
-// освобождение выделенной памяти
+// freeing allocated memory
     free(arr);
     return 0;
 }
@@ -46,23 +46,23 @@ void move_zeros(int *arr, int size) {
     int *temp = (int*) malloc(size * sizeof(int));
     int i, j = 0;
 
-// копирование ненулевых элементов из arr в temp
+// copying non-zero elements from arr to temp
     for (i = 0; i < size; i++) {
         if (arr[i] != 0) {
             temp[j++] = arr[i];
         }
     }
-    // копирование нулевых элементов из arr в temp
+   // copying zero elements from arr to temp
     for (i = 0; i < size; i++) {
         if (arr[i] == 0) {
             temp[j++] = arr[i];
         }
     }
-    // копирование элементов из temp обратно в arr
+   // copying elements from temp back to arr
     for (i = 0; i < size; i++) {
         arr[i] = temp[i];
     }
-// освобождение памяти, выделенной под временный массив
+// Freeing memory allocated for a temporary array
     free(temp);
 }
 
